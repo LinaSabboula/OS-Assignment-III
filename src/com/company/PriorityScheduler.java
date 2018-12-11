0ac0e9b7f2f5629f8cff6341454d69a61f6c7fe9
Lina Sabboula <linamuhab@gmail.com>
2018-12-11 20:05:43 +0200

    implemented findHighestPriority function and class constructor

--- src/com/company/PriorityScheduler.java
+++ src/com/company/PriorityScheduler.java
@@ -1,16 +1,44 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class PriorityScheduler {
-    private ArrayList<Process> processArray = new ArrayList<Process>();
+    private Process[] processes;
+    private ArrayList<Process> processQueue = new ArrayList<Process>();
     int currentTime = 0;
     int waitingTime = 0;
     int turnAroundTime = 0;
+    static int index = 0;
+    int processesCount;
 
-    void addProcess(Process p){}
+    //arr should be sorted based on arrival time
+    PriorityScheduler(Process[] arr){
+        processes = new Process[arr.length];
+        for(int i = 0;i < arr.length;i++){
+            processes[i] = arr[i];
+        }
+        processesCount = arr.length;
+    }
+    void startScheduler(){
+        while(processesCount != 0){
+        }
+    }
+    void addProcess(){}
 
-    int findHighestPriority(){}
+    int findHighestPriority(){
+        int min = processQueue.get(0).getProcessPriority();
+        int priorityIndex = 0;
+        for(int i = 1;i < processQueue.size();i++){
+            if((processQueue.get(i).getProcessPriority()) < min){
+                min = processQueue.get(i).getProcessPriority();
+                priorityIndex = i;
+            }
+        }
+        return priorityIndex;
+    }
 
     Process extractProcess(int index){}
 
