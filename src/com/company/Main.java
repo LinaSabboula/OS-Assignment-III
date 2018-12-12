912d0d6374178ba6b355c58cf05d4ed50cd734f2
Lina Sabboula <linamuhab@gmail.com>
2018-12-12 20:17:22 +0200

    Changed processList to a static array, and sorting it according to arrivalTime before using in any schedulers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,50 +1,59 @@
 package com.company;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	Scanner s = new Scanner(System.in);
-	System.out.println("Enter Number of process: ");
-	int n= s.nextInt();
-	System.out.println("Enter RR Quantum: ");
-	int rq = s.nextInt();
-	System.out.println("Enter Context Switch Time: ");
-	int t = s.nextInt();
-	ArrayList <Process> processList= new ArrayList<Process>();
-	for(int i=0;i<n;i++) {
-        String name;
-        int arrivalTime, burstTime,priority, quantum;
-        System.out.println("Enter name of process" + i+1 +" : "  );
-        name= s.next();
-        System.out.println("Enter Arrival Time of process" + i+1 +" : "  );
-        arrivalTime= s.nextInt();
-        System.out.println("Enter Burst Time of process" + i+1 +" : "  );
-        burstTime= s.nextInt();
-        System.out.println("Enter Priority of process" + i+1 +" : "  );
-        priority= s.nextInt();
-        System.out.println("Enter Quantum of process" + i+1 +" : "  );
-        quantum=s.nextInt();
-        Process p = new Process(name,arrivalTime,burstTime,priority,quantum);
-	    processList.add(p);
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter Number of process: ");
+        int n = s.nextInt();
+        System.out.print("\nEnter RR Quantum: ");
+        int rq = s.nextInt();
+        System.out.print("\nEnter Context Switch Time: ");
+        int t = s.nextInt();
+        Process[] processList = new Process[n];
+        for (int i = 0; i < n; i++) {
+            String name;
+            int arrivalTime, burstTime, priority, quantum;
+            System.out.print("\nEnter name of process " + (i + 1) + " : ");
+            name = s.next();
+            System.out.print("\nEnter Arrival Time of process " + (i + 1) + " : ");
+            arrivalTime = s.nextInt();
+            System.out.print("\nEnter Burst Time of process " + (i + 1) + " : ");
+            burstTime = s.nextInt();
+            System.out.print("\nEnter Priority of process " + (i + 1) + " : ");
+            priority = s.nextInt();
+            System.out.print("\nEnter Quantum of process " + (i + 1) + " : ");
+            quantum = s.nextInt();
+            Process p = new Process(name, arrivalTime, burstTime, priority, quantum);
+            processList[i] = p;
+        }
+        Process temp;
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j > 0; j--) {
+                if (processList[j].getArrivalTime() < processList[j - 1].getArrivalTime()) {
+                    temp = processList[j];
+                    processList[j] = processList[j - 1];
+                    processList[j - 1] = temp;
+                }
+            }
         }
 
 
-System.out.println("SRJF: " );
-    // Call function
+        System.out.println("SRJF: ");
+        // Call function
 
-System.out.println("RR: " );
-    // Call function
+        System.out.println("RR: ");
+        // Call function
 
-System.out.println("Priority Scheduling: ");
-    //Call function
+        System.out.println("Priority Scheduling: ");
+        //Call function
 
-System.out.println("AG Scheduling: ");
-    //Call function
-
-
-
+        System.out.println("AG Scheduling: ");
+        //Call function
 
     }
 }
+
+
+
